34403620e7f0bc3c76e4e7db959079def8ce4c0e
ryancolorcafe <ryan.brown@fastradius.com>
2022-10-10 10:34:27 -0700

    creat useCount hook, throw error if no context

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -5,6 +5,14 @@ import * as React from 'react'
 
 const CountContext = React.createContext()
 
+const useCount = () => {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error('useCount must be used within a CountProvider')
+  }
+  return context
+}
+
 const CountProvider = props => {
   const [count, setCount] = React.useState(0)
   const value = [count, setCount]
@@ -12,12 +20,12 @@ const CountProvider = props => {
 }
 
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
